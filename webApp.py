6c6d5f8c0816cc92759ebe36926da3d591c003d5
chiatsekuo <jonathan.kuo33@gmail.com>
2021-04-22 13:27:46 +0800

    streamlit app done

--- webApp.py
+++ webApp.py
@@ -4,21 +4,42 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from PIL import Image
 import streamlit as st
+import numpy as np
+
 
 st.write("""
 # Stroke Detection
-Detect if someon has stroke using machine learning!
+Detect if someone has stroke using machine learning!
 """)
 
-image = Image.open('./health.jpg')
-st.image(image, caption='ML', use_column_width=True)
+# image = Image.open('./health.jpg')
+# st.image(image, caption='Stay Healthy!', use_column_width=True)
+
+st.subheader("Table information")
+numpy_data = np.array(
+    [['Female', 'Male'],
+    ['No', 'Yes'],
+    ['Rural', 'Urban'],
+    ['No', 'Yes'],
+    ['No', 'Yes']])
+dataframe = pd.DataFrame(data=numpy_data, index=["gender", "ever married", "residence type", 'hypertension', 'heart disease'], columns=["0", "1"])
+st.table(dataframe)
+
+numpy_data = np.array(
+    [['Unknown', 'formerly smoked','never smoked','smokes']])
+dataframe = pd.DataFrame(data=numpy_data, index=["smoking status"], columns=["0", "1", "2", "3"])
+st.table(dataframe)
+
+numpy_data = np.array(
+    [['Govt job', 'Never worked','Private','Self-employed','children']])
+dataframe = pd.DataFrame(data=numpy_data, index=["work type"], columns=["0", "1", "2", "3", "4"])
+st.table(dataframe)
+
+
 
 df = pd.read_csv('stroke.csv')
 
-st.subheader('Data info:')
-st.dataframe(df)
-st.write(df.describe())
-chart = st.bar_chart(df)
+
 
 X = df.iloc[:, 0:10].values
 y = df.iloc[:, -1].values
@@ -69,5 +90,12 @@ st.write(str(accuracy_score(y_test, RandomForestClassifier.predict(X_test))*100)
 
 prediction = RandomForestClassifier.predict(user_input)
 
-st.subheader('Classification: ')
+st.subheader('**Classification**:')
+st.write('(note: stroke: 1, healthy: 0)')
 st.write(prediction)
+
+
+st.subheader('Data info:')
+st.dataframe(df)
+st.write(df.describe())
+chart = st.bar_chart(df)
